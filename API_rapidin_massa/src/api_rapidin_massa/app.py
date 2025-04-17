49c67f4557166fd28dd72ba474cd6e2d6fbe1780
Andrey Coutinho <148481213+Androka2004@users.noreply.github.com>
2025-04-17 14:05:45 +0000

    Criando rotas CRUD (aula 3)

--- API_rapidin_massa/src/api_rapidin_massa/app.py
+++ API_rapidin_massa/src/api_rapidin_massa/app.py
@@ -1,8 +1,66 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+
+from api_rapidin_massa.schemas import (
+    DatabasePublico,
+    Message,
+    UserDB,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def lendo_raiz():
     return {'message': 'Raios Funde'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def criando_usuario(user: UserSchema):
+    usuario_id_incluso = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(usuario_id_incluso)
+
+    return usuario_id_incluso
+
+
+@app.get('/users/', response_model=DatabasePublico)
+def trazendo_usuarios():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def atualizando_usuarios(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    usuario_id_incluso = UserDB(id=user_id, **user.model_dump())
+
+    database[user_id - 1] = usuario_id_incluso
+
+    return usuario_id_incluso
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def pulverizando_usuarios(user_id: int, password: str):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    if database[user_id - 1].password == password:
+        database.pop(user_id - 1)
+
+        return {'message': 'usuario deletado'}
+
+    else:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED, detail='Wrong password'
+        )
--- API_rapidin_massa/tests/test_app.py
+++ API_rapidin_massa/tests/test_app.py
@@ -1,14 +1,66 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from api_rapidin_massa.app import app
-
-
-def test_ler_root_ok():
-    client = TestClient(app)
-
+def test_ler_root_ok(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Raios Funde'}
+
+
+def test_criador_de_usuario(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Raios Funde',
+            'password': '12345',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Raios Funde',
+        'id': 1,
+        'email': 'test@test.com',
+    }
+
+
+def test_leitor_database(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Raios Funde',
+                'id': 1,
+                'email': 'test@test.com',
+            }
+        ]
+    }
+
+
+def test_alterador_usuario(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Funde Raios',
+            'password': '12345',
+            'email': 'lomba@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Funde Raios',
+        'id': 1,
+        'email': 'lomba@test.com',
+    }
+
+
+def test_pulverizar_teste(client):
+    response = client.delete('/users/1', params={'password': '12345'})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'usuario deletado'}
